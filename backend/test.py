6a081ee90f300c0d3c20dcc084bbfa1d752d5e59
evlaleyla <136358189+evlaleyla@users.noreply.github.com>
2024-06-06 22:02:34 +0200

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -4,7 +4,6 @@ import pandas as pd
 import logging
 
 app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
-
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -25,6 +24,48 @@ def index():
 @app.route('/maps', methods=['GET'])
 def maps():
     return render_template('maps.html')
+
+
+@app.route('/pivot_charts', methods=['GET', 'POST'])
+def pivot_charts():
+    if request.method == 'POST':
+        data_type = request.form.get('data_type')
+        x_coord = request.form.get('x_coord', 'default_x')
+        y_coord = request.form.get('y_coord', 'default_y')
+
+        logging.debug(f"Received POST data: data_type={data_type}, x_coord={x_coord}, y_coord={y_coord}")
+
+        try:
+            conn = get_db_connection()
+            if data_type == 'pizza':
+                table = 'pizza_profit_analysis'
+                query = f"SELECT SKU, Name AS Category, Size, Price, Profit FROM {table}"
+            elif data_type == 'restaurant':
+                table = 'total_items_sold'
+                query = f"SELECT {x_coord}, {y_coord} FROM {table}"
+            elif data_type == 'profit':
+                table = 'pizza_profit_analysis'
+                query = f"SELECT {x_coord}, {y_coord} FROM {table}"
+            elif data_type == 'monthly_revenue':
+                table = 'monthly_store_revenue'
+                x_coord = 'orderMonth'
+                y_coord = 'totalRevenue'
+                query = f"SELECT {x_coord}, {y_coord}, storeID FROM {table}"
+            else:
+                return jsonify({"error": "Invalid data type"}), 400
+
+            df = pd.read_sql(query, conn)
+
+            chart_data = df.to_dict(orient='records')
+            logging.debug(f"Chart data: {chart_data}")
+            return jsonify(chart_data=chart_data)
+        except Exception as e:
+            logging.error(f"Error in /pivot_charts endpoint: {e}")
+            return render_template('error.html', error_message="Error fetching chart data")
+        finally:
+            conn.close()
+
+    return render_template('pivot_charts.html')
 
 @app.route('/store_locations', methods=['GET'])
 def store_locations():
